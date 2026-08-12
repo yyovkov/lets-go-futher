cfa866ea5f9a6d14a9cbed4dfa700e78aef5952a
Yovko Yovkov <yyovkov@yyovkov.net>
2026-08-12 18:07:46 +0300

    Chapter 19.05: Building binaries

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"expvar"
 	"flag"
+	"fmt"
 	"log/slog"
 	"os"
 	"runtime"
@@ -15,9 +16,12 @@ import (
 	_ "github.com/lib/pq"
 	"greenlight.yyovkov.net/internal/data"
 	"greenlight.yyovkov.net/internal/mailer"
+	"greenlight.yyovkov.net/internal/vcs"
 )
 
-const version = "1.0.0"
+var (
+	version = vcs.Version()
+)
 
 type config struct {
 	port int
@@ -76,7 +80,15 @@ func main() {
 		cfg.cors.trustedOrigins = strings.Fields(val)
 		return nil
 	})
+
+	displayVersion := flag.Bool("version", false, "Display version and exit")
+
 	flag.Parse()
+
+	if *displayVersion {
+		fmt.Printf("Version:\t%s\n", version)
+		os.Exit(0)
+	}
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
---
+++ internal/vcs/vcs.go
@@ -0,0 +1,12 @@
+package vcs
+
+import "runtime/debug"
+
+func Version() string {
+	bi, ok := debug.ReadBuildInfo()
+	if ok {
+		return bi.Main.Version
+	}
+
+	return ""
+}
